72491662b58f5f0a11f811bc6a705deb63100e5a
MichaelLifshitsL1 <mlifshits@loyalty.com>
2019-01-07 17:15:23 -0500

    use xray

--- src/main/java/com/loyalty/poc/xray/Handler.java
+++ src/main/java/com/loyalty/poc/xray/Handler.java
@@ -2,14 +2,24 @@ package com.loyalty.poc.xray;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.xray.AWSXRay;
 
 import java.util.Map;
 
-public class Handler implements RequestHandler<Map<String,Object>, String> {
+public class Handler implements RequestHandler<Map<String, Object>, String> {
 
     @Override
-    public String handleRequest(Map<String,Object> input, Context context) {
+    public String handleRequest(Map<String, Object> input, Context context) {
+
         System.out.println("+++++++++++++++input = [" + input + "], context = [" + context + "]");
+
+        System.out.println("+++++++++++++++AWSXRay.getCurrentSegment()="+AWSXRay.getCurrentSegment());
+
+        // wrap in subsegment
+        AWSXRay.beginSubsegment("New Subsegment start");
+        System.out.println("+++++++++++++++Subsegment");
+        AWSXRay.endSubsegment();
+
         return "+++OK";
     }
 }
